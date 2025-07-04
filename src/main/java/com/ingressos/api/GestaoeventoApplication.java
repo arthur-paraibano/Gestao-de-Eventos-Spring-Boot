39f3c42fea9199a7f12523673bfb2485536d04d0
Arthur Mikael <arthurmik860@gmail.com>
2025-07-04 15:02:39 -0300

    feat [ Project created with folders ]

---
+++ src/main/java/com/ingressos/api/GestaoeventoApplication.java
@@ -0,0 +1,12 @@
+package com.ingressos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoeventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoeventoApplication.class, args);
+	}
+}
