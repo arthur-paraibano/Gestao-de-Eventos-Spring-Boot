d5fd19624ef8980d529036a3208f5dec4781b58e
Arthur Mikael <arthurmik860@gmail.com>
2025-07-05 19:24:23 -0300

    feat [ Order Service Created ]

---
+++ src/main/java/com/ingressos/api/service/OrderService.java
@@ -0,0 +1,40 @@
+package com.ingressos.api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ingressos.api.dto.GeneralDto;
+import com.ingressos.api.exceptions.IncorrectParameterException;
+import com.ingressos.api.exceptions.IsNullException;
+import com.ingressos.api.model.OrderModel;
+import com.ingressos.api.repository.OrderRepository;
+import com.ingressos.api.util.InternationalizationUtil;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository repository;
+    @Autowired
+    private InternationalizationUtil messageInternationalization;
+
+    @Transactional(readOnly = true)
+    public List<OrderModel> findAll() {
+        return repository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public OrderModel findById(GeneralDto dto) {
+        if (dto == null || dto.id() == null) {
+            throw new IsNullException(messageInternationalization.getMessage("id.null"));
+        }
+        return repository.findById(dto.id())
+                .orElseThrow(() -> new IncorrectParameterException(
+                        messageInternationalization.getMessage("descriptions.order.notfound")));
+    }
+}
